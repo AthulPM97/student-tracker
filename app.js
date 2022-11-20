418494f8ca030aae878ac093b232174520b0c8fb
athulpm <97athulpm@gmail.com>
2022-11-20 17:35:52 +0530

    Added basic html and js skeleton

---
+++ app.js
@@ -0,0 +1,13 @@
+//selectors
+const studentForm = document.getElementById('studentForm');
+const studentDiv = document.querySelector('.students');
+const nameInput = studentForm['name'];
+const ageInput = studentForm['age'];
+const rollInput = studentForm['rollNo'];
+
+
+//event listeners
+
+
+
+//functions
